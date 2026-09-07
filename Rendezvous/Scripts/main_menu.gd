5b04fed94d4161bd7f62908b619133a52b67b37d
Zach9123 <zach24l@gisboyshigh.net>
2026-09-08 09:51:41 +1200

    V2.15
    Finished main menu, except for options button. Started on better tile sheet and obstacles

--- Rendezvous/Scripts/main_menu.gd
+++ Rendezvous/Scripts/main_menu.gd
@@ -2,19 +2,20 @@ extends Node2D
 
 @onready var start_sprite: AnimatedSprite2D = $Button_Manager/StartButton/Start
 @onready var options_sprite: AnimatedSprite2D = $Button_Manager/OptionsButton/Options
+@onready var end_sprite: AnimatedSprite2D = $"Button_Manager/End Button/End"
 
 func _ready() -> void:
-	# Set both sprites to use the "On" animation
 	start_sprite.animation = "On"
 	options_sprite.animation = "On"
+	end_sprite.animation = "On"
 	
-	# Jump exactly to frame 1 (or 0 for the first image)
 	start_sprite.frame = 1
 	options_sprite.frame = 1
+	end_sprite.frame = 1
 	
-	# Stop them from playing so they stay frozen on that frame until hovered
 	start_sprite.stop()
 	options_sprite.stop()
+	end_sprite.stop()
 
 # --- Start Button ---
 func _on_start_button_mouse_entered() -> void:
@@ -23,9 +24,25 @@ func _on_start_button_mouse_entered() -> void:
 func _on_start_button_mouse_exited() -> void:
 	start_sprite.play("Off")
 
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+
 # --- Options Button ---
 func _on_options_button_mouse_entered() -> void:
 	options_sprite.play("On")
 
 func _on_options_button_mouse_exited() -> void:
 	options_sprite.play("Off")
+
+func _on_options_button_pressed() -> void:
+	print("Settings menu will load here!")
+
+# --- End Button ---
+func _on_end_button_mouse_entered() -> void:
+	end_sprite.play("On")
+
+func _on_end_button_mouse_exited() -> void:
+	end_sprite.play("Off")
+
+func _on_end_button_pressed() -> void:
+	get_tree().quit()
